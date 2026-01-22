d47c3c46cdc88d777c1884781f3b7ef54e45f7a0
Rishit Parija <rishitparija774@gmail.com>
2026-01-22 16:36:48 +0530

    Java file created by sts

---
+++ demo_sts_javaproject/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package demo_sts_javaproject;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second Project");
+	}
+
+}
